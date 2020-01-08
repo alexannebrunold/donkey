0974e472f83566e6c2a618f859f3d54b788183b2
Alexanne BRUNOLD <alexannebrunold@MacBook-Pro-de-Alexanne.local>
2020-01-08 17:12:15 +0100

    Collision, score game & end

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 var isJumping = false;
 var posJumping = 0;
+var value
 var jumpList = [44, 22, 11, 11, -11, -11, -22, -44];
 x = 120;
 y = 120;
@@ -9,6 +10,17 @@ oxo.inputs.listenKeyOnce("enter", function () {
   oxo.screens.loadScreen("game", function () {
     var character = document.querySelector('.player');
     var ennemy = document.querySelector('.obstacle1');
+    var affichageScore = document.querySelector('.affichageScore');
+
+    //Score
+    value = 0;
+    setInterval(function () {
+      value++;
+    },
+      1000
+    );
+
+    affichageScore.innerHTML = value;
 
     setInterval(function doGame() {
       if (isJumping) {
@@ -28,7 +40,12 @@ oxo.inputs.listenKeyOnce("enter", function () {
 
     //Collision
     oxo.elements.onCollisionWithElement(character, ennemy, function () {
-      oxo.screens.loadScreen("end");
+      oxo.screens.loadScreen("end", function () {
+        var affichageScore = document.querySelector('.affichageScore');
+
+        affichageScore.innerHTML = value;
+
+      });
     });
   });
 
@@ -46,9 +63,8 @@ oxo.inputs.listenKeyOnce("enter", function () {
     d.style.left = x_player + "px";
     d.style.bottom = y_player + "px";
   }
-
-
 });
+
 
 
 
