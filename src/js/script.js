a4605890b5c27403ddddd59898f92da502b23eab
bertrand jesenberger <bjesen@MacBook-Air-de-bertrand.local>
2020-01-08 10:49:56 +0100

    jump

---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+
+var isJumping = false;
+var posJumping = 0;
+var jumpList = [44,22,11,11,-11,-11,-22,-44];
+x = 120;
+y = 120;
+
+oxo.inputs.listenKeyOnce('enter', function() {
+    oxo.screens.loadScreen('game');
+});
+
+
+oxo.inputs.listenKey('space', function() {
+    console.log('espace');
+    if (oxo.screens.getCurrentScreen() == 'game') {
+        if (!isJumping) {
+            console.log('jump !');
+            isJumping = true;
+        }
+    }
+});
+
+
+function placeDiv(x_pos, y_pos) {
+    var d = document.querySelector('.player');
+    d.style.position = "absolute";
+    d.style.left = x_pos + 'px';
+    d.style.bottom = y_pos + 'px';
+}
+
+setInterval(function doGame() {
+    if (isJumping) {
+        vplayer = document.querySelector('.player');
+        ecart = jumpList[posJumping];
+        y += ecart;
+        placeDiv(x, y);
+        posJumping++;
+        if (posJumping == jumpList.length) {
+            isJumping = false;
+            posJumping = 0;
+        }
+    } 
+}, 50);
+
