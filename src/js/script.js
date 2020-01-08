a51d584f67b9c39023ad371480d9274f4ed2ddf7
Alexanne BRUNOLD <alexannebrunold@MacBook-Pro-de-Alexanne.local>
2020-01-08 15:42:34 +0100

    Collision

--- src/js/script.js
+++ src/js/script.js
@@ -5,37 +5,50 @@ x = 120;
 y = 120;
 x_obst = 1268;
 
-oxo.inputs.listenKeyOnce("enter", function() {
-  oxo.screens.loadScreen("game");
-  oxo.inputs.listenKey("space", function() {
+oxo.inputs.listenKeyOnce("enter", function () {
+  oxo.screens.loadScreen("game", function () {
+    var character = document.querySelector('.player');
+    var ennemy = document.querySelector('.obstacle1');
+
+    setInterval(function doGame() {
+      if (isJumping) {
+        vplayer = document.querySelector(".player");
+        ecart = jumpList[posJumping];
+        y += ecart;
+        placePlayer(x, y);
+        posJumping++;
+        if (posJumping == jumpList.length) {
+          isJumping = false;
+          posJumping = 0;
+        }
+      }
+      x_obst -= 10;
+      ennemy.style.left = x_obst + "px";
+    }, 50);
+
+    oxo.elements.onCollisionWithElement(player, obstacle1, function () {
+      // Character is touched by ennemy
+      //If player is touched by obstacle1 --> open  end page 
+      oxo.screens.loadScreen("end");
+    });
+  });
+  oxo.inputs.listenKey("space", function () {
     if (oxo.screens.getCurrentScreen() == "game") {
       if (!isJumping) {
         isJumping = true;
       }
     }
   });
-  
+
   function placePlayer(x_player, y_player) {
     var d = document.querySelector(".player");
     d.style.position = "absolute";
     d.style.left = x_player + "px";
     d.style.bottom = y_player + "px";
   }
-  
-  setInterval(function doGame() {
-    if (isJumping) {
-      vplayer = document.querySelector(".player");
-      ecart = jumpList[posJumping];
-      y += ecart;
-      placePlayer(x, y);
-      posJumping++;
-      if (posJumping == jumpList.length) {
-        isJumping = false;
-        posJumping = 0;
-      }
-    }
-    var obst = document.querySelector('.obstacle1');
-    x_obst -= 10; 
-    obst.style.left = x_obst + "px";
-  }, 50);
+
+
 });
+
+//Collision
+
